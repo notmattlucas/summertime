13d5a1fcbb956a2473f550c970879ed2f056fda7
Matthew Lucas <lucasmatthew@hotmail.com>
2018-08-03 07:36:35 +0100

    Started addition of modal

--- src/components/Catalogue.vue
+++ src/components/Catalogue.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="catalogue">
     <ul v-for="entry in entries" v-bind:key="entry.id">
-      <li>{{entry.title}}</li>
+      <li><button>{{entry.title}}</button></li>
+      <Modal/>
     </ul>
   </div>
 </template>
 
 <script>
 
+import Modal from './Modal'
+
 export default {
   name: 'Catalogue',
+  components: {
+    Modal
+  },
   props: {
     entries: {
       type: Array,
